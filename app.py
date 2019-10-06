415bc0a2108135a02c0a2d9cd42855ed95359488
wanlapa111 <56195470+wanlapa111@users.noreply.github.com>
2019-10-06 21:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,12 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    if (userText == 'สวัสดี) :
+       sendText(user,'เออ ดีด้วย')
+    elif (userText == 'ไปนะ') :
+       sendText(user,'ไปตายไหนก็ไป')
+    else :
+       sendText(user,'ว่าไงนะ')
     return '',200
 
 def sendText(user, text):
